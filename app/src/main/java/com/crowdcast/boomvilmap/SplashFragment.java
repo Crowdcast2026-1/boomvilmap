2cc07487e6ddfbf16caadc12e6b52ef3b8bb76de
sukjin123 <tjrwls5661@naver.com>
2026-06-01 21:06:07 +0900

    feat: 스플래시 진행 애니메이션과 로그인 이동 구현

---
+++ app/src/main/java/com/crowdcast/boomvilmap/SplashFragment.java
@@ -0,0 +1,43 @@
+package com.crowdcast.boomvilmap;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SplashFragment extends Fragment {
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private int progress = 0;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        ProgressBar progressBar = view.findViewById(R.id.progress_loading);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                progress += 2;
+                progressBar.setProgress(progress);
+                if (progress >= 100) {
+                    handler.postDelayed(() -> ((MainActivity) requireActivity()).showLogin(), 300);
+                } else {
+                    handler.postDelayed(this, 40);
+                }
+            }
+        };
+        handler.post(runnable);
+    }
+}
