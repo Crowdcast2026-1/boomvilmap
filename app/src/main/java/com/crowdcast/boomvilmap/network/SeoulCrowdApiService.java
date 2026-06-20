9cc59fb48cd5044867addf2e277ad6aeb204db27
sukjin123 <tjrwls5661@naver.com>
2026-06-21 01:49:03 +0900

    feat(network): add weather API endpoint

--- app/src/main/java/com/crowdcast/boomvilmap/network/SeoulCrowdApiService.java
+++ app/src/main/java/com/crowdcast/boomvilmap/network/SeoulCrowdApiService.java
@@ -3,6 +3,7 @@ package com.crowdcast.boomvilmap.network;
 import com.crowdcast.boomvilmap.model.CollectAllResponse;
 import com.crowdcast.boomvilmap.model.CurrentPopulationResponse;
 import com.crowdcast.boomvilmap.model.PredictionResponse;
+import com.crowdcast.boomvilmap.model.WeatherResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -27,4 +28,13 @@ public interface SeoulCrowdApiService {
 
     @POST("collect/all")
     Call<CollectAllResponse> getAllPopulation();
+
+    @GET("https://api.open-meteo.com/v1/forecast")
+    Call<WeatherResponse> getCurrentWeather(
+            @Query("latitude") double latitude,
+            @Query("longitude") double longitude,
+            @Query("current") String currentVariables,
+            @Query("timezone") String timezone,
+            @Query("wind_speed_unit") String windSpeedUnit
+    );
 }
